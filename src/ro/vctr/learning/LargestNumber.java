cd92997f519cc8a00a20abfb3a668a32898e9a70
Victor Ro <vctr.ro@gmail.com>
2021-12-18 18:43:33 +0400

    Added LargestNumber

---
+++ src/ro/vctr/learning/LargestNumber.java
@@ -0,0 +1,15 @@
+package ro.vctr.learning;
+
+public class LargestNumber {
+
+    public static int largestNumberIndex(int[] nums) {
+        int index = -1;
+
+        return index;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(largestNumberIndex(new int[]{1, 24, 4, 8, 3, 6, 8}));
+    }
+}
