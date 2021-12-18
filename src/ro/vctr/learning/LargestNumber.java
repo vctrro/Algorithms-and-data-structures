68a86fce9c576b942086413b80b13056e13105ca
Victor Ro <vctr.ro@gmail.com>
2021-12-18 18:43:33 +0400

    Added LargestNumber

---
+++ src/ro/vctr/learning/LargestNumber.java
@@ -0,0 +1,18 @@
+package ro.vctr.learning;
+//You are given an integer array nums where the largest integer is unique.
+//Determine whether the largest element in the array is at least twice as much as every other number in the array.
+// If it is, return the index of the largest element, or return -1 otherwise.
+public class LargestNumber {
+
+    public static int largestNumberIndex(int[] nums) {
+        int index = -1;
+
+
+        return index;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(largestNumberIndex(new int[]{1, 24, 4, 8, 3, 6, 8}));
+    }
+}
