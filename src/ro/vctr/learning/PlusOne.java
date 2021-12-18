ffade19e8a98dcbad382d79cdf8c842687d0c264
Victor Ro <vctr.ro@gmail.com>
2021-12-18 23:15:33 +0400

    Added PlusOne

---
+++ src/ro/vctr/learning/PlusOne.java
@@ -0,0 +1,17 @@
+package ro.vctr.learning;
+
+import java.util.Arrays;
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+// The digits are ordered from most significant to least significant in left-to-right order.
+// The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[]{1, 1, 4, 8, 3, 6, 8})));
+    }
+}
