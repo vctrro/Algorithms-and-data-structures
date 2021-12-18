031405244136d32b0070a7f6f56a52a69a4b2adf
Victor Ro <vctr.ro@gmail.com>
2021-12-18 23:18:00 +0400

    Solved PlusOne

--- src/ro/vctr/learning/PlusOne.java
+++ src/ro/vctr/learning/PlusOne.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
 public class PlusOne {
 
     public static int[] plusOne(int[] digits) {
-
+        digits[digits.length-1]++;
+        return digits;
     }
 
     public static void main(String[] args) {
